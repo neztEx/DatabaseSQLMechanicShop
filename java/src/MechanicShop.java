9428240b2f2c58cc9f874c9b2c5748b94b1b36e3
cristina95138 <lawson.cristina@gmail.com>
2020-08-25 21:36:41 -0700

    WIP

--- java/src/MechanicShop.java
+++ java/src/MechanicShop.java
@@ -474,6 +474,7 @@ public class MechanicShop{
 	}
 	
 	public static void ListCustomersWithBillLessThan100(MechanicShop esql){//6
+	    //completed by CristinaL
 	    try{
 	        String query = "SELECT c.fname, c.lname, r.date, r.comment, r.bill FROM c.customer, r.closed_request WHERE r.bill < 100";
 		    esql.executeQueryAndPrintResult(query);
@@ -487,6 +488,7 @@ public class MechanicShop{
 	}
 	
 	public static void ListCarsBefore1995With50000Milles(MechanicShop esql){//8
+	    //completed by CristinaL
 		try{
 		    String query = "SELECT c.make, c.model, c.year FROM c.car, r.service_request WHERE c.vin = r.car_vin AND c.year < 1995 AND r.odometer < 50000";
 		    esql.executeQueryAndPrintResult(query);
